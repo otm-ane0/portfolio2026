29d2c4005dbdb76f206785d0487e01251c3893ba
otm-ane0 <hammadiotmane21@gmail.com>
2026-05-08 17:17:28 +0100

    GOATGUARD

--- src/components/HackathonDetailModal.jsx
+++ src/components/HackathonDetailModal.jsx
@@ -30,8 +30,8 @@ const highlights = [
 
 const links = {
     devfolio: 'https://devfolio.co/projects/base-realms-b63a',
-    github: 'https://github.com/zickrian/Base-Realms',
-    live: 'https://baserealms.app/',
+    github: 'https://github.com/otm-ane0',
+    live: '',
     docs: 'https://docs.google.com/document/d/1smjwXkiQOyB1mvwBO683Me2Dcbd6aGKBdlfJGjURSjU/edit?usp=sharing',
 };
 
